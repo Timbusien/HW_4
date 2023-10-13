b15d7c3877e268209c9b5efffa434d0280c83c57
Timbusien <timbusiennavi@gmail.com>
2023-10-14 00:22:31 +0500

    ljb

--- main.py
+++ main.py
@@ -110,11 +110,3 @@ while True:
         print(db)
         break
 
-    # if z == 'c1':
-    #     print(calc1(a = input('число 1: '), b = input('число 2: ')))
-    #
-    # elif z == 'c2':
-    #     print(calc2(a = input('число 1: '), b = input('число 2: ')))
-    #
-    # elif z == 'c3':
-    #     print(calc3(a = input('число 1: '), b = input('число 2: ')))
