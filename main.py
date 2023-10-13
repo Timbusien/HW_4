85626c520ba1dbd17239c1a2a4c01b83f90b09f7
Timbusien <timbusiennavi@gmail.com>
2023-10-14 00:24:49 +0500

    patch note 2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class User(BaseModel):
     address: str
 
 
-class Bakns(BaseModel):
+class Banks(BaseModel):
     bank_name: str
     rating: str
     opened_in: str
@@ -17,7 +17,7 @@ class Bakns(BaseModel):
 
 class Cards(BaseModel):
     cardholder: User
-    which_bank: Bakns
+    which_bank: Banks
     opened_card: str
 
 
@@ -58,56 +58,79 @@ while True:
     z = input('Введите опцию: ')
     if z.lower() == 'заполнение':
 
-        try:
-            user = User(name=input('введите имя: '),
-                        mail=input('введите вашу почту: '),
-                        address=input('введите ваш адрес: '))
-            print(user)
-            db.append(user)
+         try:
+             name = input('введите ваше имя: ')
+             mail = input('введите вашу почту: ')
+             address = input('введите ваш адресс ')
+             bank_name = input('введите название банка которым пользуетесь: ')
+             rating = input('введите его рейтинг банка: ')
+             opened_in = input('введите дату когда последний раз пользовались услугами банка: ')
+             cardholder = User
+             which_bank = Banks
+             opened_card = input('введите дату регистрации вашей карты: ')
+             card = Cards
+             amount = int(input('введите ваш счёт: '))
+             currency = input('введите вашу валюту: ')
 
-            bank = Bakns(bank_name=input('введите название вашего банка: '),
-                         rating=input('введите рейтинг банка: '),
-                         opened_in=input('дата вашей регистрации в этом банке: '))
-            print(bank)
-            db.append(bank)
+             user = User(name=name,
+                         mail=mail,
+                         address=address,
+                         bank_name=bank_name,
+                         rating=rating,
+                         opened_in=opened_in,
+                         opened_card=opened_card,
+                         amount=amount,
+                         currency=currency)
 
-            card = Cards(cardholder=user,
-                         which_bank=bank,
-                         opened_card=input('дата регистрации вашей карты: '))
-            print(card)
-            db.append(card)
-
-            balance = Balance(card=card,
-                              ammount=int(input('введите ваш счёт: ')),
-                              currency=input('введите валюту: '))
-            print(balance)
-            db.append(balance)
-
-        except:
+             db.append({name:dict(user)})
+    #         user = User(name=input('введите имя: '),
+    #                     mail=input('введите вашу почту: '),
+    #                     address=input('введите ваш адрес: '))
+    #         print(user)
+    #         db.append(user)
+    #
+    #         bank = Bakns(bank_name=input('введите название вашего банка: '),
+    #                      rating=input('введите рейтинг банка: '),
+    #                      opened_in=input('дата вашей регистрации в этом банке: '))
+    #         print(bank)
+    #         db.append(bank)
+    #
+    #         card = Cards(cardholder=user,
+    #                      which_bank=bank,
+    #                      opened_card=input('дата регистрации вашей карты: '))
+    #         print(card)
+    #         db.append(card)
+    #
+    #         balance = Balance(card=card,
+    #                           ammount=int(input('введите ваш счёт: ')),
+    #                           currency=input('введите валюту: '))
+    #         print(balance)
+    #         db.append(balance)
+    #
+         except:
             logging.basicConfig(format="%(asctime)s | %(messages)s", filename='errors.log')
             logging.warning('There is a mistake')
             print('ERROR')
-
+    #
     elif z.lower() == 'банковские операции':
         x = input('введите одну из банковых операций: ')
 
         if x.lower() == 'пополнить баланс':
             c = int(input('введите сумму пополнения: '))
 
-            print(f'Ваш счёт :{calc1(balance.amount, c)}')
+            print(f'Ваш счёт: {calc1(amount, c)}')
 
         elif x.lower() == 'снять деньги':
             c = int(input('введите суммы снятия: '))
 
-            print(f'Ваш счёт :{calc2(balance.amount, c)}')
+            print(f'Ваш счёт: {calc2(amount, c)}')
 
         elif x.lower() == 'умножение счёта':
             c = int(input('введите сумму умножения: '))
 
-            print(f'Ваш счёт : {calc3(balance.amount, c)}')
+            print(f'Ваш счёт: {calc3(amount, c)}')
 
     elif z == 'stop':
         print(db)
         break
 
-)
