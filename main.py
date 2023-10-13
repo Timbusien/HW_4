75783b94b01c66cf8390390c3d4ee192bcd4bdb4
Rustam Abdurashitov <143092592+timbusien@users.noreply.github.com>
2023-10-14 00:19:16 +0500

    kljhfg

--- main.py
+++ main.py
@@ -1,40 +1,120 @@
-import time
-def sync_traffic_light():
-    while True:
-        print("Красный")
-        time.sleep(5)
-        print("Желтый")
-        time.sleep(2)
-        print("Зеленый")
-        time.sleep(5)
+import logging
+# import pydantic
+from pydantic import BaseModel
 
-sync_traffic_light()
 
-import asyncio
+class User(BaseModel):
+    name: str
+    mail: str
+    address: str
 
-async def async_traffic():
-    while True:
-        print("Красный")
-        await asyncio.sleep(5)
-        print("Желтый")
-        await asyncio.sleep(2)
-        print("Зеленый")
-        await asyncio.sleep(5)
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(async_traffic())
+class Bakns(BaseModel):
+    bank_name: str
+    rating: str
+    opened_in: str
 
-import threading
-import time
 
-def sync_light():
-    while True:
-        print("Красный")
-        time.sleep(5)
-        print("Желтый")
-        time.sleep(2)
-        print("Зеленый")
-        time.sleep(5)
+class Cards(BaseModel):
+    cardholder: User
+    which_bank: Bakns
+    opened_card: str
 
-thread = threading.Thread(target=sync_light)
-thread.start()
+
+class Balance(BaseModel):
+    card: Cards
+    amount: int = 0
+    currency: str
+
+
+db = []
+
+
+def calc1(a: int, b: int) -> dict:
+    try:
+        return a + b
+    except TypeError:
+        logging.basicConfig(format="%(asctime)s | %(messages)s", filename='errors.log')
+        logging.warning('There is a mistake')
+
+
+def calc2(a: int, b: int) -> dict:
+    try:
+        return a - b
+    except TypeError:
+        logging.basicConfig(format="%(asctime)s | %(messages)s", filename='errors.log')
+        logging.warning('There is a mistake')
+
+
+def calc3(a: int, b: int) -> dict:
+    try:
+        return a * b
+    except TypeError:
+        logging.basicConfig(format="%(asctime)s | %(messages)s", filename='errors.log')
+        logging.warning('There is a mistake')
+
+
+while True:
+    z = input('Введите опцию: ')
+    if z.lower() == 'заполнение':
+
+        try:
+            user = User(name=input('введите имя: '),
+                        mail=input('введите вашу почту: '),
+                        address=input('введите ваш адрес: '))
+            print(user)
+            db.append(user)
+
+            bank = Bakns(bank_name=input('введите название вашего банка: '),
+                         rating=input('введите рейтинг банка: '),
+                         opened_in=input('дата вашей регистрации в этом банке: '))
+            print(bank)
+            db.append(bank)
+
+            card = Cards(cardholder=user,
+                         which_bank=bank,
+                         opened_card=input('дата регистрации вашей карты: '))
+            print(card)
+            db.append(card)
+
+            balance = Balance(card=card,
+                              ammount=int(input('введите ваш счёт: ')),
+                              currency=input('введите валюту: '))
+            print(balance)
+            db.append(balance)
+
+        except:
+            logging.basicConfig(format="%(asctime)s | %(messages)s", filename='errors.log')
+            logging.warning('There is a mistake')
+            print('ERROR')
+
+    elif z.lower() == 'банковские операции':
+        x = input('введите одну из банковых операций: ')
+
+        if x.lower() == 'пополнить баланс':
+            c = int(input('введите сумму пополнения: '))
+
+            print(f'Ваш счёт :{calc1(balance.amount, c)}')
+
+        elif x.lower() == 'снять деньги':
+            c = int(input('введите суммы снятия: '))
+
+            print(f'Ваш счёт :{calc2(balance.amount, c)}')
+
+        elif x.lower() == 'умножение счёта':
+            c = int(input('введите сумму умножения: '))
+
+            print(f'Ваш счёт : {calc3(balance.amount, c)}')
+
+    elif z == 'stop':
+        print(db)
+        break
+
+    # if z == 'c1':
+    #     print(calc1(a = input('число 1: '), b = input('число 2: ')))
+    #
+    # elif z == 'c2':
+    #     print(calc2(a = input('число 1: '), b = input('число 2: ')))
+    #
+    # elif z == 'c3':
+    #     print(calc3(a = input('число 1: '), b = input('число 2: ')))
